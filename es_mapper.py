f59320c3999cd020226563b3f81ef7c2b3a62414
Venkat Korapaty <vkorapaty@admins-MacBook-Pro.local>
2021-08-05 16:48:10 -0400

    can now specify which indices to create and delete

--- es_mapper.py
+++ es_mapper.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import logging
 from elasticsearch import Elasticsearch
 import json
@@ -54,19 +55,29 @@ def create_index(client, index_name, index_mappings):
 
 if __name__ == "__main__":
     esURL = os.environ.get("ES")
-    mappings = scan_directory("./mappings")
+    mappings = [mapping.split("/")[-1] for mapping in scan_directory("./mappings")]
 
-    if esURL == None: 
+    if esURL == None:
         logger.error("ES URL not specified")
         exit(-1)
+
+    if len(sys.argv) > 1:
+        indices = sys.argv[1:]
+        indices = [mapping_file + ".json" for mapping_file in indices]
+        print(indices)
+        for json_file in indices:
+            if json_file not in mappings:
+                logger.error(f"JSON file {json_file} does not exist")
+                exit(-1)
+        mappings = indices
 
     client = Elasticsearch([esURL])
 
     for mapping_file in mappings:
-        index_name = mapping_file.split("/")[-1].split(".")[0]
+        index_name = mapping_file.split(".")[0]
         logger.info(f"Creating index {index_name}")
 
-        with open(mapping_file, 'r') as F:
+        with open("./mappings/" + mapping_file, 'r') as F:
             content = F.read()
             content = json.loads(content)
             create_index(client, index_name, content)
